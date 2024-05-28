50a7fea421f7fbaaea6f32bff55f1ce9572cc353
Puneethkumar <puneethkumarg96@gmail.com>
2024-05-28 18:36:20 +0530

    lab10

---
+++ prog_10/10a.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Time</title>
+    </head>
+    <body>
+        <h1>Today's date is
+        <date>
+            <?php
+            $date = date("y-m-d");
+            echo $date;
+            ?>
+        </date>
+        </h1>
+    </body>
+</html>
